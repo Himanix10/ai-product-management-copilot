f77489cc9e96569bda91b03161c5ee8efefd076f
Pradeepthi-yarlagadda <pradeepthi297@gmail.com>
2026-08-11 22:29:24 +0530

    Delete __init__ (3).py

--- __init__ (3).py
+++
@@ -1,3 +0,0 @@
-from .db import get_db_connection, init_db, fetch_customer_feedback_db, insert_customer_feedback_db
-
-__all__ = ["get_db_connection", "init_db", "fetch_customer_feedback_db", "insert_customer_feedback_db"]
